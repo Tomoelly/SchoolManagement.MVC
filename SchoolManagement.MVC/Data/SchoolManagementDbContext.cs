e787169c4213444e994e27cedd4e3a612faebee7
Tomoelly <ellyhuang26@gmail.com>
2023-11-02 10:03:49 +0800

    Add Generator

---
+++ SchoolManagement.MVC/Data/SchoolManagementDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagement.MVC.Data;
+
+public partial class SchoolManagementDbContext : DbContext
+{
+    public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Course__3214EC07227FEA2A");
+
+            entity.ToTable("Course");
+
+            entity.HasIndex(e => e.Code, "UQ__Course__A25C5AA7C25D2911").IsUnique();
+
+            entity.Property(e => e.Code).HasMaxLength(5);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Lecture>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC073662719F");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07A2B63372");
+
+            entity.Property(e => e.DateOfBirth).HasColumnType("date");
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- SchoolManagement.MVC/Program.cs
+++ SchoolManagement.MVC/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolManagement.MVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//黑人添加:
+
+//1. when the application starts up, grab me the connection string
+//builder 在上面有var by電腦
+//.configuration>>會去找所有app settings
+//使用GetConnectionString方法
+var conn= builder.Configuration.GetConnectionString("SchoolManagementDbConnection");
+
+//2. 用剛剛那個ConnectionString
+//開始建立跟資料庫“實際的”連線
+//這個DB context是一個model of
+//後面說的這個是該用的connection string (conn)
+builder.Services.AddDbContext<SchoolManagementDbContext>(q=>q.UseSqlServer(conn));
+//黑人添加止
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
